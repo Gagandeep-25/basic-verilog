c2a3bead8907ecd0fecfa3cb4da318992fb55a7e
Gagandeep.L.S <gagandeepls7777@gmail.com>
2025-08-09 20:04:34 +0530

    Update T_flipflop.v

--- T_flipflop.v
+++ T_flipflop.v
@@ -14,3 +14,41 @@ module T_flipflop(
         end
     end
 endmodule
+
+
+
+
+
+`timescale 1ns / 1ps
+module T_flipflop_tb;
+reg T, clk;
+wire Q, Q_bar;
+
+ T_flipflop uut (
+    .T(T),
+    .clk(clk),
+    .Q(Q),
+    .Q_bar(Q_bar)
+  );
+  
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk; 
+  end
+  
+  initial begin
+    $dumpfile("T_flipflop_tb.vcd");
+    $dumpvars(0, T_flipflop_tb);
+
+   
+    T = 0; #10; 
+    T = 1; #10; 
+    T = 0; #10; 
+    T = 1; #10; 
+    T = 0; #10;
+    T = 1; #10; 
+    #20;
+    $finish;
+  end
+endmodule
+
